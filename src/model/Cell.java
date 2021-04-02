8ed8653d91f50b6753a03aa84bda3dafd1196eb4
Lady_Proger <facktartar@mail.com>
2021-04-02 12:01:32 +0300

    1_3 add cell class realization

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,7 +1,8 @@
-package Model;
+package model;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Cell {
 
@@ -15,17 +16,26 @@ public class Cell {
 
 	private static int index = 0;
 
+	// спросить как их инициализировать
 	private ArrayList<Cell> neighbors = new ArrayList<>();
 
 
-	public void Cell(Point point) {
-
+	public Cell(Point point) {
+		this.cellPosition = point;
+		this.letter = '\0';
 	}
 
 	public void updateCellState() {
 		switch (this.cellState){
+			case CELL_IS_EMPTY -> this.cellState = CellState.CELL_WITH_SETTED_LETTER_AT_TURN;
+			case CELL_WITH_SETTED_LETTER_AT_TURN -> this.cellState = CellState.CELL_IS_SELECTED;
+			case CELL_IS_SELECTED -> this.cellState = CellState.CELL_IS_BUSY;
+			// fixme return to me
+			default -> this.cellState = CellState.CELL_IS_EMPTY;
 		}
 	}
+
+	//todo update method with returning new state
 
 	public void setLetter(char letter) {
 		this.letter = letter;
@@ -39,16 +49,29 @@ public class Cell {
 		this.selectedIndex = Cell.index++;
 	}
 
+	public int getSelectedIndex() {return this.selectedIndex;}
+
 	public void resetCell() {
 		this.cellState = CellState.CELL_IS_EMPTY;
 		this.letter = '\0';
 	}
 
 	public void revertCellState(){
-		switch (this.cellState){}
+		switch (this.cellState){
+			case CELL_IS_SELECTED:
+				this.cellState = this.letter != '\0'? CellState.CELL_IS_BUSY : CellState.CELL_IS_EMPTY;
+				break;
+			case CELL_WITH_SETTED_LETTER_AT_TURN:
+				this.cellState = CellState.CELL_IS_EMPTY;
+				this.letter = '\0';
+				break;
+			default:
+				//do something
+
+		}
 	}
 
-	public ArrayList getNeighbors() {
+	public List getNeighbors() {
 		return new ArrayList(this.neighbors);
 	}
 
