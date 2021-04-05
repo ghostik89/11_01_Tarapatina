1985124b42fa2fdd89cb6f9082da5abeffcb00a7
Lady_Proger <facktartar@mail.com>
2021-04-05 11:41:32 +0300

    1_3 complete first realization for gamefield

--- src/model/GameField.java
+++ src/model/GameField.java
@@ -1,4 +1,6 @@
 package model;
+import org.jetbrains.annotations.NotNull;
+
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -8,14 +10,28 @@ import java.util.stream.Collectors;
 
 public class GameField {
 
-	private ArrayList<Cell> playFiled;
+	private final ArrayList<Cell> playFiled = new ArrayList<>();
 	private Cell letterSettedAtTurn;
 
 	public GameField(int width, int height) {
-		for(int i = 0; i < width; i++)
-			for(int j = 0; j < height; j++)
-				playFiled.add(new Cell(new Point(i,j)));
-		//todo adding neighbor here
+		if(width < 3 || height < 3)
+			throw new IllegalArgumentException();
+
+		for(int x = 0; x < width; x++)
+			for(int y = 0; y < height; y++)
+				this.playFiled.add(new Cell(new Point(x,y)));
+
+		for(int x = 0; x < width; x++)
+			for(int y = 0; y < height; y++){
+				if(x > 0)
+					this.getCellByPoint(new Point(x,y)).setNeighbor(this.getCellByPoint(new Point(x-1,y)));
+				if(y > 0)
+					this.getCellByPoint(new Point(x,y)).setNeighbor(this.getCellByPoint(new Point(x,y-1)));
+				if(y < height-1)
+					this.getCellByPoint(new Point(x,y)).setNeighbor(this.getCellByPoint(new Point(x,y+1)));
+				if(x < width - 1)
+					this.getCellByPoint(new Point(x,y)).setNeighbor(this.getCellByPoint(new Point(x+1,y)));
+			}
 	}
 
 	public int getWidth() {
@@ -30,39 +46,50 @@ public class GameField {
 				.max().orElseThrow(NoSuchElementException::new);
 	}
 
-	public Cell getCellByPoint(Point point) {
+	public Cell getCellByPoint(@NotNull Point point) {
 		return this.playFiled.stream()
 				.filter(elem -> elem.getCellPosition().equals(point))
 				.findAny()
 				.orElse(null);
 	}
 
-	public void setLetterAtTurn(char letter, Cell cell) {
-		if(cell != null){
-			cell.setLetter(letter);
-			this.letterSettedAtTurn = cell;
-		}
+	public void setCellForLetterAtTurn(@NotNull Cell cell) {
+		this.letterSettedAtTurn = cell;
+		cell.updateCellState();
 	}
 
-	public void selectCell(Cell cell) {
-		//fixme maybe this useless
-		if(cell != null){
-			cell.updateCellState();
-		}
+	public void setCharIntoCellAtTurn(char letter){
+		this.letterSettedAtTurn.setLetter(letter);
 	}
 
-	public boolean isAvailableCell(Point point, GameState gameState) {
+	public void selectCell(@NotNull Cell cell) {
+		cell.updateCellState();
+		cell.setIndex();
+	}
+
+	public boolean isAvailableCell(@NotNull Point point,@NotNull GameState gameState) {
 		boolean isAvailable = false;
+		Cell cell = this.getCellByPoint(point);
 
-		switch (gameState){
-			case PLAYER_INSERTING_LETTER:
-				//do something
-				break;
-			case PLAYER_SELECTING_CHARS:
-				//do something
-				break;
-			default:
-				//do something
+		if (gameState == GameState.PLAYER_SELECT_CELL_FOR_INSERT_LETTER) {
+			for (Cell elem : cell.getNeighbors())
+				if (elem.getCellState() == CellState.CELL_IS_BUSY) {
+					isAvailable = true;
+					break;
+				}
+		} else if (gameState == GameState.PLAYER_SELECTING_CHARS) {
+			if (this.getAllSelectedCells().isEmpty())
+				for (Cell elem : cell.getNeighbors())
+					if (elem.getCellState() == CellState.CELL_IS_BUSY) {
+						isAvailable = true;
+						break;
+					} else {
+						for (Cell newElem : cell.getNeighbors())
+							if (newElem.getCellState() == CellState.CELL_IS_SELECTED) {
+								isAvailable = true;
+								break;
+							}
+					}
 		}
 
 		return isAvailable;
@@ -88,6 +115,22 @@ public class GameField {
 	public void cleanFieldAfterPlayersTurn() {
 		this.getAllSelectedCells().forEach(Cell::updateCellState);
 		this.letterSettedAtTurn = null;
+	}
+
+	public void clearAll(){
+		for (Cell elem : this.playFiled)
+			elem.resetCell();
+	}
+
+	public boolean isFullField(){
+		boolean isFull = true;
+		for(Cell elem : this.playFiled)
+			if (elem.getCellState() == CellState.CELL_IS_EMPTY) {
+				isFull = false;
+				break;
+			}
+
+		return isFull;
 	}
 
 }
