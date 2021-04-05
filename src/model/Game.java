1c6f33dfff5ae0113ecb2176aafc0ed170b791e6
Lady_Proger <facktartar@mail.com>
2021-04-05 11:41:14 +0300

    1_3 complete first realization for game

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,55 +1,95 @@
 package model;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Game {
 
 	private GameState currentState;
 
-	private Player players;
+	private int currentPlayerIndex = 0;
 
 	private Player currentPlayer;
 
-	private ArrayList<Player> player;
+	private ArrayList<Player> players;
 
-	private Alphabet alphabet;
+	public Game(GameField field) throws FileNotFoundException {
+		Alphabet alphabet = new Alphabet("абвгдеёжзийклмнопрстуфхцчшщъыьэюя");
+		WordManager wordManager = new WordManager();
+		Player player1 = new Player(wordManager, "Player 1", alphabet, field);
+		Player player2 = new Player(wordManager, "Player 2", alphabet, field);
 
-	public void Game(GameField field) {
-
+		this.currentState = GameState.PLAYER_SELECT_CELL_FOR_INSERT_LETTER;
+		this.players.add(player1);
+		this.players.add(player2);
+		this.currentPlayer = player1;
 	}
 
 	public void changePlayer() {
-
+		this.currentPlayerIndex = this.currentPlayerIndex == 1 ? 0 : 1;
+		this.currentPlayer = this.players.get(this.currentPlayerIndex);
 	}
 
 	public String determinateWinner() {
-		return null;
-	}
+		String winner = null;
 
-	public void endGame() {
+		if(this.currentPlayer.getGameFiled().isFullField())
+			winner = this.players.get(0).calcScore() > this.players.get(1).calcScore()?
+					this.players.get(0).getName() : this.players.get(1).getName();
 
+		return winner;
 	}
 
 	public Player getCurrentPlayer() {
 		return this.currentPlayer;
 	}
 
-	public static GameState getCurrentState() {
-		return null;
+	public GameState getCurrentState() {
+		return this.currentState;
 	}
 
 	public void updateCurrentState() {
-
+		switch (this.currentState){
+			case PLAYER_SELECT_CELL_FOR_INSERT_LETTER -> {
+				this.currentState = GameState.PLAYER_INSERTING_LETTER;
+			}
+			case PLAYER_INSERTING_LETTER -> {
+				this.currentState = GameState.PLAYER_SELECTING_CHARS;
+			}
+			case PLAYER_SELECTING_CHARS -> {
+				this.currentState = GameState.PLAYER_SUBMITTED_TURN;
+			}
+			case PLAYER_SUBMITTED_TURN -> {
+				this.currentState = GameState.PLAYER_SELECT_CELL_FOR_INSERT_LETTER;
+			}
+		}
 	}
 
 	public void revertState() {
+		switch (this.currentState){
+			case PLAYER_INSERTING_LETTER -> {
+				this.currentState = GameState.PLAYER_SELECT_CELL_FOR_INSERT_LETTER;
+			}
+			case PLAYER_SELECTING_CHARS -> {
+				this.currentState = GameState.PLAYER_INSERTING_LETTER;
+			}
+			case PLAYER_SUBMITTED_TURN -> {
+				this.currentState = GameState.PLAYER_SELECTING_CHARS;
+			}
+		}
+	}
 
+
+	public void endGame() {
+		this.currentPlayer.getWordManager().clearAll();
+		this.currentPlayer.getGameFiled().clearAll();
 	}
 
 }
 
 
 enum GameState {
+	PLAYER_SELECT_CELL_FOR_INSERT_LETTER,
 	PLAYER_INSERTING_LETTER,
 	PLAYER_SELECTING_CHARS,
 	PLAYER_SUBMITTED_TURN
