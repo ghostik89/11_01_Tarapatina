f3ea5eba07549d49b6e213d79467cfe6db9f048e
Lady_Proger <facktartar@mail.com>
2021-04-05 11:41:41 +0300

    1_3 complete first realization for player

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,47 +1,70 @@
 package model;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.awt.*;
 
 public class Player {
 
-	private String name;
+	private final String name;
 
-	private GameField gameField;
+	private final GameField gameField;
 
-	private WordManager wordManager;
+	private final WordManager wordManager;
 
-	private Alphabet alphabet;
+	private final Alphabet alphabet;
+
+	public Player(@NotNull WordManager wordManager,@NotNull String name,
+				  @NotNull Alphabet alphabet,@NotNull GameField gameField) {
+		this.wordManager = wordManager;
+		this.name = name;
+		this.alphabet = alphabet;
+		this.gameField = gameField;
+	}
 
 	public int calcScore() {
-		return 0;
+		return wordManager
+				.getAllSolvedWordsByPlayer(this)
+				.stream().sequential().map(String::length).reduce(0, Integer::sum);
 	}
 
 	public String getName() {
-		return null;
+		return this.name;
 	}
 
-	public void insertLetterIntoCell(Point point) {
+	public void selectCellForInsertLetter(Point point){
+		this.gameField.getCellByPoint(point).updateCellState();
+	}
 
+	public void insertLetterIntoCell(char letter) {
+		if(!this.alphabet.letterHasInAlphabet(letter))
+			this.gameField.setCharIntoCellAtTurn(letter);
+		else
+			throw new IllegalArgumentException();
 	}
 
 	public void selectCell(Point point) {
-
+		this.gameField.selectCell(this.gameField.getCellByPoint(point));
 	}
 
-	public String submitTurn() {
-		return null;
-	}
+	public SubmitState submitTurn() {
+		SubmitState submitState = SubmitState.TURN_SUCCESS;
+		String word = this.gameField.getWordSettedAtTurn();
 
-	public void Player(WordManager wordManager, String name) {
+		if(!(wordManager.hasWordInDictionary(word)))
+			submitState = SubmitState.WORDMANAGER_ERROR_NOT_FOUND;
+		else if(!wordManager.addToSolvedWords(this, word))
+			submitState = SubmitState.WORDMANAGER_ERROR_IS_SOLVED;
 
+		return submitState;
 	}
 
 	public GameField getGameFiled() {
-		return null;
+		return this.gameField;
 	}
 
-	public WordManager getWordManage() {
-		return null;
+	public WordManager getWordManager() {
+		return this.wordManager;
 	}
 
 }
