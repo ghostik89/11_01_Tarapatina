b7ef0c3ee08a4dcdeb5844f89760ce558becb40c
Lady_Proger <facktartar@mail.com>
2021-04-08 01:02:39 +0300

    1_3 add javadoc

--- src/model/Alphabet.java
+++ src/model/Alphabet.java
@@ -8,18 +8,31 @@ public class Alphabet {
 
 	private String container;
 
+	/**Конструктор класса
+	 * @param alphabet алфавит, который будет использоваться в игре
+	 * */
 	public Alphabet(@NotNull String alphabet) {
 		this.container = alphabet;
 	}
 
+	/**Геттер алфавита
+	 * @return текущий алфавит
+	 * */
 	public String getCurrentAlphabet() {
 		return this.container;
 	}
 
-	public void setCurrentAlphabet(String alphabet) {
+	/**Сеттер текущего алфавита
+	 * @param alphabet новый алфавит
+	 * */
+	public void setCurrentAlphabet(@NotNull String alphabet) {
 		this.container = alphabet;
 	}
 
+	/**Проверка на наличие символа в алфавите
+	 * @param letter символ, который нужно проверить на наличие
+	 * @return находится ли он внутри алфавита
+	 * */
 	public boolean letterHasInAlphabet(char letter){
 		return this.container.toLowerCase(Locale.ROOT).indexOf(letter) != -1;
 	}
