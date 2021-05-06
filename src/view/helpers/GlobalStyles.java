46fbf5890f0a0f0e668be63f3c1403027cae2d01
Lady_Proger <facktartar@mail.com>
2021-05-06 15:45:05 +0300

    1_6 [init] create global styles

---
+++ src/view/helpers/GlobalStyles.java
@@ -0,0 +1,14 @@
+package view.helpers;
+
+import java.awt.*;
+
+public interface GlobalStyles {
+    Color PRIMARY_COLOR = Color.black;
+    Color PRIMARY_BACKGROUND_COLOR = Color.white;
+    Color BORDER_COLOR = Color.decode("#646464");
+    Color SELECTED_CELL = Color.decode("#656769");
+    Color INSERTED_LETTER = Color.decode("#9A9898");
+    Color INSERTED_BACKGROUND = Color.decode("#CFCFCF");
+    Font HEADER_FONT = new Font("Roboto", Font.BOLD, 20);
+    Font MAIN_FONT = new Font("Roboto", Font.PLAIN, 16);
+}
