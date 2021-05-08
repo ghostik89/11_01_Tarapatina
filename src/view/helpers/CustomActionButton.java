54c89c65d15c094f32a82047aca525382932f547
Lady_Proger <facktartar@mail.com>
2021-05-09 00:19:04 +0300

    1_6 [feat] remove unused code

--- src/view/helpers/CustomActionButton.java
+++ src/view/helpers/CustomActionButton.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CustomActionButton extends JButton {
-    private final Dimension arcs = new Dimension(10,10);
 
     public CustomActionButton(String text){
         super(text);
@@ -17,11 +16,8 @@ public class CustomActionButton extends JButton {
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        int width = getWidth();
-        int height = getHeight();
         Graphics2D graphics = (Graphics2D) g;
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         graphics.setColor(getBackground());
-        graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
     }
 }
