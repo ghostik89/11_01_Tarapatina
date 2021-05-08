94d325e75ff890dad8cad08205304f8dc4eb8dfa
Lady_Proger <facktartar@mail.com>
2021-05-09 00:19:58 +0300

    1_6 [init] create entry point for ui

---
+++ src/BaldaGame.java
@@ -0,0 +1,9 @@
+import view.MainWindow;
+
+import javax.swing.*;
+
+public class BaldaGame {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(MainWindow::new);
+    }
+}
