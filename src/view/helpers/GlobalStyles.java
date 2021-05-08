929eec63fb022ab748e986f683a568daeab55e12
Lady_Proger <facktartar@mail.com>
2021-05-09 00:17:46 +0300

    1_6 [feat] add new font

--- src/view/helpers/GlobalStyles.java
+++ src/view/helpers/GlobalStyles.java
@@ -9,6 +9,7 @@ public interface GlobalStyles {
     Color SELECTED_CELL = Color.decode("#656769");
     Color INSERTED_LETTER = Color.decode("#9A9898");
     Color INSERTED_BACKGROUND = Color.decode("#CFCFCF");
-    Font HEADER_FONT = new Font("Roboto", Font.BOLD, 20);
+    Font MAIN_HEADER_FONT = new Font("Roboto", Font.BOLD, 28);
+    Font HEADER_FONT = new Font("Roboto", Font.PLAIN, 20);
     Font MAIN_FONT = new Font("Roboto", Font.PLAIN, 16);
 }
