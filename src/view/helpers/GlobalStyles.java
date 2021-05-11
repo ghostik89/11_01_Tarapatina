0954b7f8c3bafecadc1bec4327c6860d425a2518
Ekaterina Tarapatina <facktartar@gmail.com>
2021-05-11 19:03:53 +0300

    1_6 [ref] add fontName

--- src/view/helpers/GlobalStyles.java
+++ src/view/helpers/GlobalStyles.java
@@ -3,13 +3,14 @@ package view.helpers;
 import java.awt.*;
 
 public interface GlobalStyles {
+    String fontName = "Roboto";
     Color PRIMARY_COLOR = Color.black;
     Color PRIMARY_BACKGROUND_COLOR = Color.white;
     Color BORDER_COLOR = Color.decode("#646464");
     Color SELECTED_CELL = Color.decode("#656769");
     Color INSERTED_LETTER = Color.decode("#9A9898");
     Color INSERTED_BACKGROUND = Color.decode("#CFCFCF");
-    Font MAIN_HEADER_FONT = new Font("Roboto", Font.BOLD, 28);
-    Font HEADER_FONT = new Font("Roboto", Font.PLAIN, 20);
-    Font MAIN_FONT = new Font("Roboto", Font.PLAIN, 16);
+    Font MAIN_HEADER_FONT = new Font(fontName, Font.BOLD, 28);
+    Font HEADER_FONT = new Font(fontName, Font.PLAIN, 20);
+    Font MAIN_FONT = new Font(fontName, Font.PLAIN, 16);
 }
