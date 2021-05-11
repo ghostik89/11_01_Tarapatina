edc40ae12bf5c617ddb016290b6fe0d49569f71a
Ekaterina Tarapatina <facktartar@gmail.com>
2021-05-11 17:00:33 +0300

    1_6 [feat] add background on hover and on focus

--- src/view/helpers/CustomActionButton.java
+++ src/view/helpers/CustomActionButton.java
@@ -2,23 +2,28 @@ package view.helpers;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.Locale;
 
 public class CustomActionButton extends JButton {
 
-    public CustomActionButton(String text){
-        super(text);
-        setContentAreaFilled(false);
-        setFocusPainted(false);
-        setOpaque(false);
+    public CustomActionButton(String text) {
+        super(text.toUpperCase(Locale.ROOT));
         setBackground(GlobalStyles.PRIMARY_BACKGROUND_COLOR);
+        setBorder(BorderFactory.createLineBorder(GlobalStyles.BORDER_COLOR));
         addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 setBackground(GlobalStyles.INSERTED_BACKGROUND);
             }
+
+            @Override
             public void mouseExited(java.awt.event.MouseEvent evt) {
-                setBackground(GlobalStyles.PRIMARY_COLOR);
+                setBackground(GlobalStyles.PRIMARY_BACKGROUND_COLOR);
             }
         });
+        addFocusListener(new ButtonCustomFocusListener());
     }
 
     @Override
@@ -27,5 +32,20 @@ public class CustomActionButton extends JButton {
         Graphics2D graphics = (Graphics2D) g;
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         graphics.setColor(getBackground());
+    }
+
+    public class ButtonCustomFocusListener implements FocusListener {
+
+        @Override
+        public void focusGained(FocusEvent e) {
+            CustomActionButton source = (CustomActionButton) e.getSource();
+            source.setBackground(GlobalStyles.INSERTED_BACKGROUND);
+        }
+
+        @Override
+        public void focusLost(FocusEvent e) {
+            CustomActionButton source = (CustomActionButton) e.getSource();
+            source.setBackground(GlobalStyles.PRIMARY_BACKGROUND_COLOR);
+        }
     }
 }
