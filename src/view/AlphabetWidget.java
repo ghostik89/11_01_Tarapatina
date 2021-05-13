ec9b7617b61c419b0846beb9b6b64161619e42a4
Lady_Proger <facktartar@mail.com>
2021-05-13 18:39:07 +0300

    1_6 [ref] add component logic

--- src/view/AlphabetWidget.java
+++ src/view/AlphabetWidget.java
@@ -1,12 +1,75 @@
 package view;
 
+import event.PlayerActionFieldEvent;
+import event.PlayerActionFieldListener;
 import model.Alphabet;
+import model.Cell;
 import view.helpers.CustomActionButton;
+import view.helpers.GlobalStyles;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class AlphabetWidget extends JDialog {
-    private Alphabet alphabet;
-    private JButton acceptBtn = new CustomActionButton("Подтвердить");
-    private JButton cancelBtn = new CustomActionButton("Отменить");
+    private final Alphabet alphabet;
+    private final JPanel alphabetGrid = new JPanel();
+    char selectedChar;
+    private final ArrayList<PlayerActionFieldListener> actionFieldEventList = new ArrayList<>();
+
+
+    public AlphabetWidget(JFrame owner, Alphabet alphabet) {
+        super(owner, "Alphabet keyboard", true);
+        this.alphabet = alphabet;
+
+        setLocation(520,300);
+        setResizable(false);
+        getContentPane().setBackground(GlobalStyles.PRIMARY_BACKGROUND_COLOR);
+        setLayout(new BorderLayout(10,10));
+
+        initAlphabetCells();
+        add(this.alphabetGrid, BorderLayout.CENTER);
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(Box.createHorizontalGlue());
+        JButton cancelBtn = new CustomActionButton("Отменить");
+        controlPanel.add(cancelBtn);
+        controlPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        JButton acceptBtn = new CustomActionButton("Подтвердить");
+        controlPanel.add(acceptBtn);
+
+        cancelBtn.addActionListener(e -> setVisible(false));
+        acceptBtn.addActionListener(e -> {
+            setVisible(false);
+            this.fireLetterIsChoose();
+        });
+
+        add(controlPanel);
+    }
+
+    private void initAlphabetCells(){
+        int size = this.alphabet.getCurrentAlphabet().length() / 2;
+        alphabetGrid.setLayout(new GridLayout(size, size, 4, 4));
+        for(char ch : this.alphabet.getCurrentAlphabet().toCharArray()){
+            alphabetGrid.add(new LetterCellWidget(ch,size, this));
+        }
+    }
+
+    public char getSelectedChar() {
+        return selectedChar;
+    }
+
+    public void setSelectedChar(char selectedChar) {
+        this.selectedChar = selectedChar;
+    }
+
+    void addListener(PlayerActionFieldListener listener){
+        this.actionFieldEventList.add(listener);
+    }
+
+    private void fireLetterIsChoose(){
+        PlayerActionFieldEvent event = new PlayerActionFieldEvent(this);
+        event.setLetter(this.selectedChar);
+        this.actionFieldEventList.forEach(e -> e.playerClickToField(event));
+    }
 }
