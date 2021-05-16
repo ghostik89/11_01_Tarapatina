2eb09f1427c4b8176d336a5abd7932b16fb691c2
Lady_Proger <facktartar@mail.com>
2021-05-16 17:46:55 +0300

    1_6 [feat] add redraw for widget

--- src/view/LetterCellWidget.java
+++ src/view/LetterCellWidget.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class LetterCellWidget extends CustomActionButton {
     private final AlphabetWidget owner;
+    private final char myChar;
 
     public LetterCellWidget(char text, int fieldSize, AlphabetWidget owner) {
         super(String.valueOf(text));
@@ -17,14 +18,19 @@ public class LetterCellWidget extends CustomActionButton {
             case 8 -> fontSize = 35;
             default -> fontSize = 30;
         }
+        this.myChar = text;
         setFont(new Font(GlobalStyles.fontName, Font.PLAIN, fontSize));
-        addActionListener(e -> this.owner.setSelectedChar(text));
+        addActionListener(e -> {
+            this.owner.setSelectedChar(this.myChar);
+            this.owner.repaint();
+            this.owner.revalidate();
+        });
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        setBackground(getText().equals(String.valueOf(this.owner.getSelectedChar()))?
+        setBackground(this.myChar == this.owner.selectedChar?
                 GlobalStyles.SELECTED_CELL : GlobalStyles.PRIMARY_BACKGROUND_COLOR);
     }
 }
