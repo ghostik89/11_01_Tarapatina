903dcf4441e311a458cfebcb6445efea74135bf6
Lady_Proger <facktartar@mail.com>
2021-05-16 22:56:37 +0300

    1_6 [feat] cast figma textfield

--- src/model/GameField.java
+++ src/model/GameField.java
@@ -19,7 +19,7 @@ public class GameField {
 	 * @throws IllegalArgumentException если ширина или длина больше 29 или меньше 3
 	 * */
 	public GameField(int width, int height) {
-		if(width < 3 || height < 3 || width > 29)
+		if(height < 3 || width > 29 || width != height)
 			throw new IllegalArgumentException();
 		this.generateField(width, height);
 	}
@@ -29,6 +29,7 @@ public class GameField {
 	 * @param height высота поля
 	 * */
 	private void generateField(int width, int height){
+		//fixme
 		for(int x = 0; x < width; x++)
 			for(int y = 0; y < height; y++)
 				this.playFiled.add(new Cell(new Point(x,y)));
