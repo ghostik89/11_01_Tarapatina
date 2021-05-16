03fc9785f5029e58b64b0772bf3f6b8db842ee26
Lady_Proger <facktartar@mail.com>
2021-05-16 17:48:41 +0300

    1_6 [feat] combine all

--- src/view/GameWidget.java
+++ src/view/GameWidget.java
@@ -5,8 +5,7 @@ import event.PlayerActionFieldListener;
 import model.Game;
 import model.GameState;
 import org.jetbrains.annotations.NotNull;
-import view.helpers.CustomActionButton;
-import view.helpers.GlobalStyles;
+import view.helpers.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,12 +13,11 @@ import java.awt.*;
 public class GameWidget extends JPanel {
     private Game game;
     private final MainWindow owner;
-    private GameFieldWidget gameFieldWidget;
     private PlayersWidget playerOne;
     private PlayersWidget playerTwo;
-    private CustomActionButton acceptBtn = new CustomActionButton("подтвердить");
-    private CustomActionButton cancelBtn = new CustomActionButton("отменить");
-    private AlphabetWidget alphabetWidget;
+    private final CustomActionButton acceptBtn = new CustomActionButton("подтвердить");
+    private final CustomActionButton changePlayerBtn = new CustomActionButton("пропустить ход");
+    private final CustomActionButton cancelBtn = new CustomActionButton("отменить");
 
 
     public GameWidget(MainWindow owner) {
@@ -31,11 +29,13 @@ public class GameWidget extends JPanel {
     }
 
     public void initField() {
+        AlphabetWidget alphabetWidget;
         setLayout(new BorderLayout(10,10));
-        this.alphabetWidget = new AlphabetWidget(this.owner, this.game.getAlphabet());
-
-        this.gameFieldWidget = new GameFieldWidget(this, new PlayerActionObserver());
-        add(this.gameFieldWidget, BorderLayout.CENTER);
+        alphabetWidget = new AlphabetWidget(this.owner, this.game.getAlphabet());
+        PlayerActionObserver observer = new PlayerActionObserver();
+        GameFieldWidget gameFieldWidget = new GameFieldWidget(this, observer);
+        alphabetWidget.addListener(observer);
+        add(gameFieldWidget, BorderLayout.CENTER);
 
         JPanel container = new JPanel(new BorderLayout());
         JPanel playersPanel = new JPanel(new BorderLayout());
@@ -53,20 +53,56 @@ public class GameWidget extends JPanel {
         controlPanel.add(Box.createHorizontalGlue());
         controlPanel.add(this.cancelBtn);
         controlPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        controlPanel.add(this.changePlayerBtn);
+        controlPanel.add(Box.createRigidArea(new Dimension(10, 0)));
         controlPanel.add(this.acceptBtn);
 
         add(container, BorderLayout.PAGE_END);
 
         this.acceptBtn.addActionListener(e -> {
             this.game.updateCurrentState();
-            if(this.game.determinateWinner() != null)
-                this.endGame();
-            if(this.game.getCurrentState() == GameState.PLAYER_INSERTING_LETTER)
-                this.alphabetWidget.setVisible(true);
+
+            if (this.game.getCurrentState() == GameState.PLAYER_INSERTING_LETTER) {
+                alphabetWidget.setVisible(true);
+            } else if (this.game.getCurrentState() == GameState.PLAYER_SUBMITTED_TURN)
+                this.submitTurn();
         });
         this.cancelBtn.addActionListener(e -> this.game.revertState());
 
         setVisible(true);
+    }
+
+    private void initModalAddingWords(){
+        CustomModal addModal = SnackbarFactory.createBasicInfoSnackbar("Ваше слово не существует в словаре\nВы хотите его добавить?", this.owner);
+        CustomActionButton addButton = new CustomActionButton("Добавить");
+        String wordToAdd = this.game.getField().getWordSettedAtTurn();
+        addButton.addActionListener(e -> {
+            this.game.getWordManager().addWordToDictionary(wordToAdd);
+            this.submitTurn();
+        });
+        addModal.addButton(addButton);
+        addModal.setVisible(true);
+    }
+
+    private void submitTurn(){
+        switch (this.game.getCurrentPlayer().submitTurn()){
+            case TURN_SUCCESS -> {
+                if (this.game.determinateWinner() != null)
+                    this.endGame();
+                this.game.getField().cleanFieldAfterPlayersTurn();
+                this.game.updateCurrentState();
+                this.game.changePlayer();
+                repaint();
+                revalidate();
+            }
+            case WORDMANAGER_ERROR_IS_SOLVED -> SnackbarFactory
+                    .createBasicInfoSnackbar("Ваше слово уже было отгадано", this.owner)
+                    .setVisible(true);
+            case WORDMANAGER_ERROR_NOT_FOUND -> this.initModalAddingWords();
+            case GAMEFIELD_HAS_NOT_LETTER_SET_AT_TURN -> SnackbarFactory
+                    .createBasicInfoSnackbar("Среди выделенных клеток нет той новой буквы!", this.owner)
+                    .setVisible(true);
+        }
     }
 
     public void setGame(@NotNull Game game) {
@@ -87,22 +123,27 @@ public class GameWidget extends JPanel {
         @Override
         public void playerClickToField(PlayerActionFieldEvent e) {
             if(!game.getField().isAvailableCell(e.getPoint(), game.getCurrentState()))
-                // todo show modal window with invalid message
-                System.out.print("Not valid!");
+                SnackbarFactory
+                        .createBasicInfoSnackbar("Вы не можете совершить действие с клеткой", owner)
+                        .setVisible(true);
             else {
-                switch (game.getCurrentState()) {
-                    case PLAYER_SELECT_CELL_FOR_INSERT_LETTER -> game.getCurrentPlayer()
+                if (game.getCurrentState() == GameState.PLAYER_SELECT_CELL_FOR_INSERT_LETTER) {
+                    game.getCurrentPlayer()
                             .selectCellForInsertLetter(e.getPoint());
-                    case PLAYER_SELECTING_CHARS -> game.getCurrentPlayer().selectCell(e.getPoint());
-                    case PLAYER_SUBMITTED_TURN -> game.getCurrentPlayer().submitTurn();
+                } else if (game.getCurrentState() == GameState.PLAYER_SELECTING_CHARS) {
+                    game.getCurrentPlayer().selectCell(e.getPoint());
                 }
             }
+            repaint();
+            revalidate();
         }
 
         @Override
         public void playerClickOnAlphabet(PlayerActionFieldEvent e) {
             if(game.getCurrentState() == GameState.PLAYER_INSERTING_LETTER)
                 game.getCurrentPlayer().insertLetterIntoCell(e.getLetter());
+            repaint();
+            revalidate();
         }
     }
 }
