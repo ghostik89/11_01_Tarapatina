aa06d954feb8258e9dd2482633ebec23173efb0a
Ekaterina Tarapatina <facktartar@gmail.com>
2021-05-18 13:04:14 +0300

    1_6 [fix] beautify view

--- src/view/PlayersWidget.java
+++ src/view/PlayersWidget.java
@@ -11,12 +11,13 @@ import java.util.ArrayList;
 public class PlayersWidget extends JPanel {
     private final GameWidget owner;
     private final Player player;
-    private final JLabel playersScore = new JLabel();
+    private final JLabel playersScore = StyledLabelFactory.createCustomLabel("", GlobalStyles.HEADER_FONT);
     private final JPanel wordsPanel = new JPanel();
 
     public PlayersWidget(@NotNull GameWidget owner, @NotNull Player player) {
         this.owner = owner;
         this.player = player;
+        setBackground(GlobalStyles.PRIMARY_BACKGROUND_COLOR);
         setLayout(new BorderLayout(20,20));
 
         setPreferredSize(new Dimension(160,511));
@@ -28,28 +29,32 @@ public class PlayersWidget extends JPanel {
         JPanel rowLayout = new JPanel(new BorderLayout(20, 20));
         wordsPanel.setLayout(new BoxLayout(wordsPanel, BoxLayout.Y_AXIS));
         wordsPanel.setBackground(GlobalStyles.PRIMARY_BACKGROUND_COLOR);
-        namePanel.setPreferredSize(new Dimension(150,60));
-        scorePanel.setPreferredSize(new Dimension(50, 60));
 
-        rowLayout.add(namePanel, BorderLayout.LINE_START);
-        rowLayout.add(scorePanel, BorderLayout.LINE_END);
+        rowLayout.add(namePanel, BorderLayout.PAGE_START);
+        rowLayout.add(scorePanel, BorderLayout.PAGE_END);
 
         rowLayout.setPreferredSize(new Dimension(210,80));
 
         add(rowLayout, BorderLayout.PAGE_START);
         add(wordsPanel, BorderLayout.PAGE_END);
 
-        JLabel playersName = new JLabel();
-        namePanel.add(playersName, BorderLayout.SOUTH);
+        JLabel playersName = StyledLabelFactory.createCustomLabel(this.player.getName(), GlobalStyles.HEADER_FONT);
+        namePanel.add(playersName, BorderLayout.PAGE_START);
         scorePanel.add(this.playersScore, BorderLayout.CENTER);
     }
 
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        this.playersScore.setText(String.valueOf(this.player.calcScore()));
+        this.playersScore.setText("Счет: " + this.player.calcScore());
+        wordsPanel.removeAll();
         ArrayList<String> playersWords = new ArrayList<>(this.owner.getGame().getWordManager()
                 .getAllSolvedWordsByPlayer(this.player));
         playersWords.forEach(word -> wordsPanel.add(StyledLabelFactory.createBasicLabel(word)));
+
+        if(this.player == this.owner.getGame().getCurrentPlayer())
+            setBorder(BorderFactory.createLineBorder(GlobalStyles.SELECTED_CELL, 1));
+        else
+            setBorder(BorderFactory.createLineBorder(GlobalStyles.FIELD_BORDER_COLOR));
     }
 }
