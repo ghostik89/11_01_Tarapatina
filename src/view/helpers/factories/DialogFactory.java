1a6e9cf31bb185b1186dc4c92b6c4e9284bb15c4
Ekaterina Tarapatina <facktartar@gmail.com>
2021-05-20 19:18:25 +0300

    1_6 [feat] fix info modal

--- src/view/helpers/factories/DialogFactory.java
+++ src/view/helpers/factories/DialogFactory.java
@@ -1,6 +1,7 @@
 package view.helpers.factories;
 
 import org.jetbrains.annotations.NotNull;
+import view.helpers.GlobalStyles;
 import view.helpers.components.CustomActionButton;
 import view.helpers.components.CustomModal;
 
@@ -8,7 +9,7 @@ import javax.swing.*;
 
 public class DialogFactory {
     public static CustomModal createBasicInfoSnackbar(@NotNull String message, @NotNull JFrame owner){
-        CustomModal infoModal = new CustomModal(owner,  StyledLabelFactory.createBasicLabel(message));
+        CustomModal infoModal = new CustomModal(owner,  StyledLabelFactory.createCustomLabel(message, GlobalStyles.HEADER_FONT));
         CustomActionButton okBtn = CustomActionButtonFactory.createButtonWithoutBorder("ок");
         okBtn.addActionListener(e -> infoModal.setVisible(false));
         infoModal.addButton(okBtn);
