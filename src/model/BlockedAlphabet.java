861bb7968171c41d923244871c6d34c5d008e008
Lady_Proger <facktartar@mail.com>
2021-05-31 00:02:12 +0300

    2_3 [init] create blocked alphabet

---
+++ src/model/BlockedAlphabet.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class BlockedAlphabet extends Alphabet{
+    Random random = new Random();
+    String blockedChars = "";
+    /**
+     * Конструктор класса
+     *
+     * @param alphabet алфавит, который будет использоваться в игре
+     * @throws IllegalArgumentException если алфавит состоит из букв разного алфавита или пуст
+     */
+    public BlockedAlphabet(@NotNull String alphabet) {
+        super(alphabet);
+    }
+
+    public void blockLetter(){
+        int blockIndex = random.nextInt(this.getCurrentAlphabet().length() + 1);
+        if(this.blockedChars.contains(String.valueOf(this.getCurrentAlphabet().charAt(blockIndex))))
+            blockLetter();
+        else
+            this.blockedChars += String.valueOf(this.getCurrentAlphabet().charAt(blockIndex));
+    }
+}
